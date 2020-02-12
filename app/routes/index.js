40f7f58fc92eb449883e4cad7d392527b8b52981
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-02-12 16:28:24 +0100

    FEAT: add welcome-section to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+  async model() {
+    return {
+      intro: {
+        title: "Hallo! Ich bin Marta,",
+        text: "Ich halte deine schönsten Momente mit meiner Kamera fest. Jedes Foto lebt durch deine Emotionen und Erinnerungen!",
+        imgUrl: "img/fotografin.jpg"
+      },
+      headline: {
+        title: "Berliner Fotografin | Berlin & Umgebung",
+        text: "Ich halte deine schönsten Momente mit meiner Kamera fest. Jedes Foto lebt durch deine Emotionen und Erinnerungen! Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+        button: "mehr über mich"
+      }
+    }
+  }
 }
