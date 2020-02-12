1dcc3b5e1dcff7546dc3f32742e3317e208734d9
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-02-12 17:19:17 +0100

    FEAT: add portfolio-highlights section to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,21 @@ export default class IndexRoute extends Route {
         title: "Berliner Fotografin | Berlin & Umgebung",
         text: "Ich halte deine schönsten Momente mit meiner Kamera fest. Jedes Foto lebt durch deine Emotionen und Erinnerungen! Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
         button: "mehr über mich"
+      },
+      highlights: {
+        note: "Portfolio Highlights",
+        title: "Photos from Shooting | Mein Studio",
+        imgUrls: {
+          left: "img/highlight/1.jpg",
+          top: "img/highlight/2.jpg",
+          bottom: "img/highlight/3.jpg",
+          right: "img/highlight/4.jpg"
+        },
+        photos: {
+          quantity: "85",
+          text: "Bilder",
+          button: "View Gallery"
+        }
       }
     }
   }
