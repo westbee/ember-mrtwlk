d4c913e57f0e5acfdb05c34c79dae055fc24e18d
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-02-19 22:22:49 +0100

    FIX: reset scroll on route change

--- app/router.js
+++ app/router.js
@@ -4,6 +4,10 @@ import config from './config/environment';
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
+
+  didTransition() {
+    window.scrollTo(0, 0);
+  }
 }
 
 Router.map(function() {
