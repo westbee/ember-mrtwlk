c2c37d8a7705b3884369dad4d045358cf9272287
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-03-10 18:09:23 +0100

    FEAT: add GalleryIntro component

--- app/routes/bildergalerie/portrait.js
+++ app/routes/bildergalerie/portrait.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class BildergaleriePortraitRoute extends Route {
+  async model() {
+    return {
+      name: "Porträtfotografie",
+      title: "eine kurze Geschichte über Dich",
+      description:
+        "Die Porträt-Session erzählt eine Geschichte über dich und ich versuche die Bilder als etwas Außergewöhnliches erscheinen zu lassen. Das, was deinen Charakter und deine Persönlichkeit darstellt und hervorhebt als Geschenk an dich selbst oder als Präsent für nahestehende Personen - ich zeige dich von deiner schönsten Seite!"
+    }
+  }
 }
