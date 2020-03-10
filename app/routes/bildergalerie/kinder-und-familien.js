c56eead6f2eae517ff51242d8d76a54b3e1adc12
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-03-10 18:09:23 +0100

    FEAT: add GalleryIntro component

--- app/routes/bildergalerie/kinder-und-familien.js
+++ app/routes/bildergalerie/kinder-und-familien.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class BildergalerieKinderUndFamilienRoute extends Route {
+  async model() {
+    return {
+      name: "Kinder & Familien",
+      title: "einzigartige Andenken für viele Jahre",
+      description:
+        "Erscheint ein kleiner Mensch auf Erden, ist dies der ideale Zeitpunkt, um diesen Moment einzufangen, weil Bilder einzigartige Andenken für die Zukunft sind. Egal ob klein oder groß, ob allein oder mit der ganzen Familie - in ungezwungener, lockerer Atmosphäre entstehen hier bleibende Erinnerungen für Eltern, Verwandte, Freunde und Liebende."
+    }
+  }
 }
--- app/routes/bildergalerie/neugeborene-und-babybauch.js
+++ app/routes/bildergalerie/neugeborene-und-babybauch.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class BildergalerieNeugeboreneUndBabybauchRoute extends Route {
+  async model() {
+    return {
+      name: "Neugeborene & Babybauch",
+      title: "die schönste Zeit im Leben einer Frau",
+      description:
+        "Wenn ein neues Leben heranwächst und das in einem selbst, ist das Gefühl unvergleichlich - Stolz und Vorfreude erfüllen eine werdende Mutter und bereiten sie auf das wohl größte Abenteuer ihres Lebens vor. Das sollte in einem ganz besonderen Rahmen verewigt werden! Ist dein Neugeborenes auf der Welt und noch nicht älter als 14 Tage, dann können wir wunderschöne und einmalige Fotos von deinem Baby entstehen lassen. Ein Neugeborenen-Shooting, um den Zauber dieses neuen Lebens festzuhalten!"
+    }
+  }
 }
