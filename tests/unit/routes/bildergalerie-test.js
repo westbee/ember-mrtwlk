bcbe9a7c4bce654cebd4b374c7538538d338f9ff
Adam Lukasiewicz <mr.westbee@gmail.com>
2020-03-12 17:59:52 +0100

    REFACT: fetch gallery data from API

--- tests/unit/routes/bildergalerie-test.js
+++ tests/unit/routes/bildergalerie-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | bildergalerie/portrait', function(hooks) {
+module('Unit | Route | bildergalerie', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:bildergalerie/portrait');
+    let route = this.owner.lookup('route:bildergalerie');
     assert.ok(route);
   });
 });
