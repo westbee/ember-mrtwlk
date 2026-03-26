18050feee72d8075e48a4a074c9fb59d4057723e
Adam Lukasiewicz <mr.westbee@gmail.com>
2026-03-26 11:28:04 +0100

    CHORE: clean up outdated dependencies and modernize CSS
    Remove jQuery from navbar in favor of native DOM API, drop IE 11 from
    browser targets, remove 6 unused dependencies (ember-flickity,
    ember-moment, ember-cli-moment-shim, ember-fetch, ember-welcome-page,
    ember-maybe-import-regenerator, ember-export-application-global),
    replace moment-format with a simple current-year helper, extract
    CSS custom properties for colors and fonts, and enable asset
    fingerprinting for cache-busting.

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,13 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from "@ember/object";
-import $ from 'jquery';
 
 export default class NavbarComponent extends Component {
 
   @tracked isScrolling = false;
 
   @action bgNav(){
-    $(".navbar-toggler").addClass("collapsed");
+    document.querySelector(".navbar-toggler")?.classList.add("collapsed");
   }
 }
