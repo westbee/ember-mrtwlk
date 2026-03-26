bc7e85f255d92ce75d06b0558667fa01d031154b
Adam Lukasiewicz <mr.westbee@gmail.com>
2026-03-26 11:20:25 +0100

    CHORE: update galleries and content

--- app/routes/bildergalerie.js
+++ app/routes/bildergalerie.js
@@ -3,12 +3,15 @@ import { inject as service } from '@ember/service';
 
 export default class BildergalerieRoute extends Route {
   @service store;
+  @service router;
 
   async model() {
     return this.store.findAll('gallery');
   }
 
-  redirect(model,transition) {
-    this.transitionTo('bildergalerie.gallery', model.get('firstObject'));
+  redirect(model, transition) {
+    if (transition.targetName === 'bildergalerie.index') {
+      this.router.transitionTo('bildergalerie.gallery', model.get('firstObject'));
+    }
   }
 }
